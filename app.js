90e0ca721fdc5a3d4036b213e2145a731f7fa05b
Mateusz Skalniak <mskalniak@gmail.com>
2020-11-16 22:00:50 +0100

    changes from lesson 7

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const addBtnElem = document.querySelector('#addBtn');
 let favouritesMovies = [];
 let lastId = 0;
+let foundMovie = null;
 
 const adverts = [
     'Nowy film: Zielona Mila, w kinach od 22 września!',
@@ -55,6 +56,7 @@ function updateFavouritesListUI() {
 
     favouritesMovies.forEach(function(movie) {
         const container = document.createElement('div');
+        container.id = `container-${movie.id}`; //TO-DO: change in other places
 
         // paragraph
         const paragraph = document.createElement('p');
@@ -63,13 +65,13 @@ function updateFavouritesListUI() {
         // delete button
         const deleteBtn = document.createElement('button');
         deleteBtn.innerText = 'Usuń';
-        deleteBtn.id = movie.id;
+        deleteBtn.id = `deleteBtn-${movie.id}`; //TO-DO: change in other places
         deleteBtn.addEventListener('click', onDeleteBtnClicked);
 
         // edit button
         const editBtn = document.createElement('button');
         editBtn.innerText = 'Edytuj';
-        editBtn.id = movie.id;
+        editBtn.id = movie.id; //TO-DO: change to editBtn-<id>
         editBtn.addEventListener('click', function() {
             console.log('id: ', movie.id);
         });
@@ -90,4 +92,47 @@ function changeAdverts() {
         let randomIndex = Math.floor(Math.random() * adverts.length);
         advertsContainer.innerText = adverts[randomIndex]; //0,1,2,3,4
     }, 2000);
+}
+
+function onSearchBtnClicked() {
+    const searchMovieNameElem = document.getElementById('searchMovieNameElem');
+
+    axios.get(`http://www.omdbapi.com/?apikey=7015f6d&t=${searchMovieNameElem.value}`).then(function(movieResponse) {
+        console.log('movie resp: ', movieResponse);
+        console.log('movie resp title: ', movieResponse.data.Title);
+
+        foundMovie = {
+            title: movieResponse.data.Title,
+            poster: movieResponse.data.Poster,
+            year: movieResponse.data.Year
+        }
+
+        const foundMovieContainer = document.getElementById('foundMovieContainer');
+        foundMovieContainer.innerHTML = `
+            <div class="movieInfo">Tytuł filmu: ${foundMovie.title}</div>
+            <div class="movieInfo">Rok produkcji: ${foundMovie.year}</div>
+            <div><img class="movieInfoPoster" src="${foundMovie.poster}" /></div>
+            <button onclick="onAddToFavourites()">Dodaj do ulubionych</button>
+        `;
+
+        // const btn = document.createElement('button');
+        // btn.innerText = "Dodaj do ulubionych";
+        // btn.addEventListener('click', function() {
+        //     console.log('found movie::: ', foundMovie);
+        // });
+        // foundMovieContainer.appendChild(btn);
+    });
+}
+
+function onAddToFavourites() {
+    let movie = {
+        name: foundMovie.title,
+        id: lastId
+    };
+    favouritesMovies.push(movie);
+    lastId++;
+
+    // UI
+    countFavouritesMovies();
+    updateFavouritesListUI();
 }
